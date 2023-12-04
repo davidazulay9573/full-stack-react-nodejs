cab48c08568fa7220ad868e621ddf40561e8cb1a
david <azulaydavid2416@gmail.com>
2023-12-04 14:50:20 +0200

    test

--- front/src/lib/api-request/http.js
+++ front/src/lib/api-request/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from 'react-toastify'
 
-const API_URL = process.env.API_URL || "http://localhost:3001"
+const API_URL = "https://fs-server.onrender.com";
 
 axios.defaults.baseURL = API_URL;
 

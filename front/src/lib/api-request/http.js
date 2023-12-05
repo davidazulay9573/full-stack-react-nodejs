b701ad420fabbb86ce3429e228e7316310848032
david <azulaydavid2416@gmail.com>
2023-12-05 14:13:05 +0200

    switch API_URL to environment variable

--- front/src/lib/api-request/http.js
+++ front/src/lib/api-request/http.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { toast } from 'react-toastify'
 
-const API_URL = "https://fs-server.onrender.com";
+const API_URL = process.env.REACT_APP_API_URL || "https://localhost:3001";
 
 axios.defaults.baseURL = API_URL;
 
 export function setCommonHeader(key, value) {
+
   axios.defaults.headers.common[key] = value;
 }
 
